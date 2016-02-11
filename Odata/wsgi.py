4b469bc022723c8a7f6ee3e38c25d7587e23c023
b3h3rkz <barjebernard@gmail.com>
2016-02-11 02:01:08 +0000

    project deployment

--- Odata/wsgi.py
+++ Odata/wsgi.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Odata.settings")
 
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
