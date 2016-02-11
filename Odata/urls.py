f8a54044d2006ddf8806de5ed0dc43367a3e1116
b3h3rkz <barjebernard@gmail.com>
2016-02-11 06:27:09 +0000

    fixed static urls

--- Odata/urls.py
+++ Odata/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Examples:
@@ -15,4 +17,4 @@ urlpatterns = [
 
 
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
