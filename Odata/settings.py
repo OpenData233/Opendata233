f267f5f58e14052b0092da3bacd4c53eaf18118b
b3h3rkz <barjebernard@gmail.com>
2016-02-11 03:16:14 +0000

    updated settings file for production

--- Odata/settings.py
+++ Odata/settings.py
@@ -145,7 +145,7 @@ DATABASES['default'].update(db_from_env)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://opendata233.herokuapp.com/']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
